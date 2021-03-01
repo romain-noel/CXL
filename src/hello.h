13cc84907a906529e15161b621564db06e2097e3
romain-noel <romainoel@free.fr>
2021-03-01 18:06:00 +0100

    first stage

---
+++ src/hello.h
@@ -0,0 +1,12 @@
+/*
+example include file
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void myPrintHelloMake(void);
+
+#ifdef __cplusplus
+} // end extern "C"
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+// Simple C++ program to display "Hello World" 
+  
+// Header file for input output functions 
+#include <iostream>  
+#include "hello.h"
+
+using namespace std; 
+  
+// main program begins 
+int main() 
+{ 
+   // call a function in another file
+   myPrintHelloMake(); 
+      
+   return(0); 
+} 
+
+//EoF
